80bb63420fa4c69220d214bee3ceeb3fc9aa9e11
Speedway <harpalassi@gmail.com>
2019-03-14 17:52:59 -0500

    added google books info

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
+require('dotenv').config()
+
 
 class App extends Component {
   render() {
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q="
+const APIKEY = "&key=" + process.env.REACT_APP_GOOGLE_BOOKS;
+
+export default {
+    searchBooks: function(query) {
+        return axios.get(BASEURL + query + APIKEY);
+    }
+}
