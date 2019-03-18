30b573c4f19f9997a16391caa5ad78391a8c99ea
Speedway <harpalassi@gmail.com>
2019-03-17 22:26:16 -0500

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Saved from "./pages/Saved";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="wrapper">
           <Navbar />
           <div className="container">
             <Route exact path="/" component={Search} />
             <Route exact path="/search" component={Search} />
             <Route exact path="/saved" component={Saved} />
           </div>
+          <Footer />
         </div>
       </Router>
     );
---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      Powered by React, MongoDB, Express, Node.js & Google Books API.
+    </footer>
+  );
+};
+
+export default Footer;
