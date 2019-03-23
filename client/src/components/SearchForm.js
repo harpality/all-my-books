be58e3deeb9e9ed3c4de0be6afc21ef04fffa8a7
Speedway <harpalassi@gmail.com>
2019-03-22 19:47:02 -0500

    mobile fonts/accessibility

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const SearchForm = props => {
   return (
     <form>
       <section className="search-form">
         <h5>Book Search</h5>
+
         <div className="input-field">
           <input
             onChange={props.handleInputChange}
             value={props.value}
             type="text"
             name="search"
+            aria-label="search"
             placeholder="Enter Book Here"
           />
         </div>
@@ -18,8 +20,7 @@ const SearchForm = props => {
           className="btn waves-effect waves-light"
           type="submit"
           name="submit"
-          onClick={props.handleFormSubmit}
-        >
+          onClick={props.handleFormSubmit}>
           Search
         </button>
       </section>
